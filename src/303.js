918f48aa8655d57b2052678e9720e3f325355728
王明全 <wangmingquan@sensorsdata.cn>
2019-09-03 18:33:21 +0800

    303

---
+++ src/303.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ */
+
+var NumArray = function(nums) {
+  this.nums = nums;
+};
+
+/**
+ * @param {number} i
+ * @param {number} j
+ * @return {number}
+ */
+
+NumArray.prototype.sumRange = function(i, j) {
+  let subArr = this.nums.slice(i, j + 1);
+  let count = 0;
+  for (let item of subArr) {
+    count += item;
+  }
+  return count;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
+// test case
+var obj = new NumArray([-2, 0, 3, -5, 2, -1]);
+console.log(obj.sumRange(0, 2));
+console.log(obj.sumRange(2, 5));
+console.log(obj.sumRange(0, 5));
