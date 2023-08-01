46bad3190a9bb8f2f612e076be62738b64d1a321
王明全 <wang.mingquan.88@gmail.com>
2023-08-01 21:34:34 +0800

    update 104

--- src/104.js
+++ src/104.js
@@ -9,6 +9,8 @@
  * @param {TreeNode} root
  * @return {number}
  */
+
+// 解法 1
 var maxDepth = function (root) {
   if (root === null) {
     return 0;
@@ -16,4 +18,26 @@ var maxDepth = function (root) {
   let left = maxDepth(root.left);
   let right = maxDepth(root.right);
   return left > right ? left + 1 : right + 1;
+};
+
+
+// 解法 2
+var maxDepth = function (root) {
+  if (root === null) {
+    return 0;
+  }
+  let layer = 0;
+  const traval = (nodes) => {
+    if (nodes.length === 0) return;
+    const _nodes = [];
+    layer++;
+    for (let i = 0, l = nodes.length; i < l; i++) {
+      const node = nodes[i];
+      if (node.left) _nodes.push(node.left);
+      if (node.right) _nodes.push(node.right);
+    }
+    traval(_nodes);
+  }
+  traval([root])
+  return layer;
 };
